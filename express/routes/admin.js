ff1f6aeb44fa2ca978b76c89c15a249264aaddda
nikolozb <berose995@gmail.com>
2023-01-09 14:28:21 +0400

    express/serving static files

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,15 +1,10 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
 
 router.get('/add-product', (req, res) => {
-	console.log('Home middleware')
-	res.send(
-		`<form action="/admin/product" method="POST">
-			<input type="text" name="title">
-			<button type="submit">Submit</button>
-		</form>`
-	)
+	res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/product', (req, res) => {
--- express/server.js
+++ express/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const adminRouter = require('./routes/admin')
 const shopRouter = require('./routes/shop')
@@ -6,12 +7,13 @@ const shopRouter = require('./routes/shop')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRouter)
 app.use('/shop', shopRouter)
 
 app.use((req, res, next) => {
-	res.status(404).send(`<h1>Page not found</h1>`)
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(4000)
