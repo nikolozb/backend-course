6377839d75e7c585b8169972fbaf517901f5d8cc
nikolozb <berose995@gmail.com>
2023-01-11 19:36:15 +0400

    sql

--- express-enhanced/server.js
+++ express-enhanced/server.js
@@ -1,24 +1,21 @@
-const path = require('path');
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const errorController = require('./controllers/error')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = express()
 
-const errorController = require('./controllers/error');
+app.set('view engine', 'ejs')
+app.set('views', process.cwd() + '/express-enhanced/views')
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.use(errorController.get404)
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
-
-app.listen(3000);
+app.listen(4000)
